49bc6fd378c028ae5786fa5055269e585b0f5ce7
Spencer Pequegnat <spencer.peq@gmail.com>
2021-01-23 10:01:18 -0800

    CRUD connected to frontend

--- api/views.py
+++ api/views.py
@@ -18,7 +18,7 @@ def apiOverview(request):
 
 @api_view(['GET'])
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-id')
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
 
